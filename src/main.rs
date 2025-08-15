bd35f3621a390423f1e8f74cad1dabc3a63b5a56
Luiz Otavio V. B. Oliveira <luiz.vbo@gmail.com>
2025-08-15 14:50:23 +0200

    Set the style manually in clap

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,33 @@
-use clap::Parser;
+use anstyle::{AnsiColor, Color, Style};
+use clap::builder::styling::Styles;
+use clap::{CommandFactory, FromArgMatches};
 use join_ai::{cli::Args, run};
 
+// Function to create the cargo-like styles
+fn get_styles() -> Styles {
+    Styles::styled()
+        .header(
+            Style::new()
+                .fg_color(Some(Color::Ansi(AnsiColor::Green)))
+                .bold(),
+        )
+        .usage(
+            Style::new()
+                .fg_color(Some(Color::Ansi(AnsiColor::Green)))
+                .bold(),
+        )
+        .literal(
+            Style::new()
+                .fg_color(Some(Color::Ansi(AnsiColor::Cyan)))
+                .bold(),
+        )
+        .placeholder(Style::new().fg_color(Some(Color::Ansi(AnsiColor::Cyan))))
+}
+
 fn main() -> anyhow::Result<()> {
-    let args = Args::parse();
+    let mut cmd = Args::command();
+    cmd = cmd.styles(get_styles());
+    let matches = cmd.get_matches();
+    let args = Args::from_arg_matches(&matches)?;
     run(args)
 }
